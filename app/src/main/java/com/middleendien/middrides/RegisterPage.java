73f6dcc7c61b354512dcbd5b90f2d5ae2ecb2f4e
Tao Peter Wang <peterwangtao0@hotmail.com>
2015-10-05 21:35:09 -0400

    Added Register Page
    Still Skeleton

---
+++ app/src/main/java/com/middleendien/middrides/RegisterPage.java
@@ -0,0 +1,17 @@
+package com.middleendien.middrides;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Peter on 10/5/15.
+ */
+public class RegisterPage extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_register);
+    }
+}
