89538421b2714b1e16aacd4d878984e8329ad221
Nosagie Asaolu <nasaolu@middlebury.edu>
2015-10-23 21:05:16 -0400

    New user request branch

--- app/src/main/java/com/middleendien/middrides/MainScreen.java
+++ app/src/main/java/com/middleendien/middrides/MainScreen.java
@@ -29,14 +29,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.middleendien.middrides.backup.AnnouncementFragment;
