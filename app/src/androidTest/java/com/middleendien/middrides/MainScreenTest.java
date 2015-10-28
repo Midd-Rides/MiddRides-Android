2d2a9be5c9d26b24352e5bb12476625b182ad3ac
Nosagie Asaolu <nasaolu@middlebury.edu>
2015-10-28 18:14:49 -0400

    Updates

---
+++ app/src/androidTest/java/com/middleendien/middrides/MainScreenTest.java
@@ -0,0 +1,17 @@
+package com.middleendien.middrides;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Nosagie on 10/27/15.
+ */
+public class MainScreenTest extends TestCase {
+
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+
+    public void testOnLocationSelected() throws Exception {
+
+    }
+}
